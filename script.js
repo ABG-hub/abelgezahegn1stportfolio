1e4eb3998054a801587597506cf62544445b58e5
ABG-hub <abel18gezahegn@gmail.com>
2024-09-05 20:00:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('fade-in'); 
-                observer.unobserve(entry.target);
-            }
-        });
-    }, {
-        threshold: 0.1 
-    });
 
-    const elementsToAnimate = document.querySelectorAll('.home, .About, .Service, .contact');
+const navbars = document.querySelectorAll(".navbar li a");
+function hover(){
+    this.classList.add("hhome");
+    console.log(this.classList);
+}
+function out(){
+    this.classList.remove("hhome");
+    console.log(this.classList);
+}
+navbars.forEach(navbar => navbar.addEventListener("mouseover",hover));
+navbars.forEach(navbar => navbar.addEventListener("mouseout",out));
 
-    elementsToAnimate.forEach(element => {
-        observer.observe(element);
-    });
+
+
+const menu = document.querySelector(".menu");
+const navbar = document.querySelector(".navbar");
+const close =document.querySelector(".close");
+menu.addEventListener("click",() => {
+    navbar.classList.add("show");
 });
+menu.addEventListener("mouseover",hover);
+menu.addEventListener("mouseout",out);
 
+close.addEventListener("click",() => {
+    navbar.classList.remove("show")
+})
+
+const icons = document.querySelectorAll(".logo i");
+
+function toggleSize(event) {
+    this.classList.toggle("fa-6x", event.type === "mouseover");
+    console.log(this.classList);
+}
+
+icons.forEach(icon => {
+    icon.addEventListener("mouseover", toggleSize);
+    icon.addEventListener("mouseout", toggleSize);
+});
 
